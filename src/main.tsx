3cf130dbbda40e0a7c875e6f4fa12fb5e6ccd7f0
Ceci Cifu <ceciliocifuentes@gmail.com>
2023-11-05 20:02:52 +0100

    style: fix element type

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom/client"
 
 import { Calendar } from "../lib"
 
-ReactDOM.createRoot(document.getElementById("root")!).render(
+ReactDOM.createRoot(document.getElementById("root") as HTMLDivElement).render(
 	<React.StrictMode>
 		<Calendar onDayClick={(day) => console.log(day)} />
 	</React.StrictMode>
