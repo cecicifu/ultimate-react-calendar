323dbe20f194a79a7ec64595396849b8362d64cf
Ceci Cifu <15237067+cecicifu@users.noreply.github.com>
2023-11-19 19:06:05 +0100

    feat: redesign (#6)

--- lib/parts/Controls/LeftIcon.tsx
+++ lib/parts/Controls/LeftIcon.tsx
@@ -6,19 +6,18 @@ export const LeftIcon = (props: Props) => {
 	return (
 		<svg
 			{...props}
-			fill="#000000"
-			viewBox="0 0 32 32"
 			xmlns="http://www.w3.org/2000/svg"
+			width="24"
+			height="24"
+			viewBox="0 0 24 24"
+			fill="none"
+			stroke="currentColor"
+			stroke-width="2"
+			stroke-linecap="round"
+			stroke-linejoin="round"
+			className="left-icon"
 		>
-			<g id="SVGRepo_bgCarrier" strokeWidth="0"></g>
-			<g
-				id="SVGRepo_tracerCarrier"
-				strokeLinecap="round"
-				strokeLinejoin="round"
-			></g>
-			<g id="SVGRepo_iconCarrier">
-				<path d="M 26 6 L 6 15.21875 L 6 16.78125 L 26 26 L 26 23.84375 L 9.46875 16 L 26 8.15625 Z"></path>
-			</g>
+			<polyline points="15 18 9 12 15 6"></polyline>
 		</svg>
 	)
 }
